3ef6273a8e79b58d6ec477e2a0ce219372b3940d
Matt Thomson <mattjohnthomson@gmail.com>
2015-09-25 20:37:00 +0100

    Extremely rough first cut

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+extern crate hyper;
+
 use std::{env, process};
 
+use hyper::Client;
+use hyper::header::{Authorization, Basic, UserAgent};
+
 fn main() {
+    let user = match env::var("GITHUB_SWEEP_USER") {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Error reading GITHUB_SWEEP_USER environment variable: {}", e);
+            process::exit(1)
+        }
+    };
+
     let token = match env::var("GITHUB_SWEEP_TOKEN") {
         Ok(val) => val,
         Err(e) => {
@@ -9,5 +22,24 @@ fn main() {
         }
     };
 
-    println!("{}", token);
+    let client = Client::new();
+
+    let auth_header = Authorization(Basic { username: user, password: Some(token) });
+    let user_agent_header = UserAgent("github-sweep".to_owned());
+
+    let request = client
+        .put("https://api.github.com/notifications")
+        .header(auth_header)
+        .header(user_agent_header)
+        .body("{}");
+
+    let response = match request.send() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Error making request: {}", e);
+            process::exit(1)
+        }
+    };
+
+    println!("{:?}", response);
 }
