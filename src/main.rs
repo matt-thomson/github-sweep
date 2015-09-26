5153fc170d40730bb0e97ffcd73e17865b3dba44
Matt Thomson <mattjohnthomson@gmail.com>
2015-09-26 15:59:52 +0100

    Make requests in a loop

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,41 @@ mod credentials;
 mod error;
 mod http;
 
-use hyper::client::Response;
-
 use std::process;
+use std::thread;
 
+use credentials::Credentials;
 use http::Client;
 use error::Error;
 
 fn main() {
-    let result = run();
+    let credentials = credentials();
+    let client = Client::new(credentials);
 
-    if result.is_err() {
-        println!("{}", result.unwrap_err());
-        process::exit(1);
+    loop {
+        let result = run(&client);
+
+        if result.is_err() {
+            println!("{}", result.unwrap_err());
+        }
+
+        thread::sleep_ms(5000);
     }
 }
 
-fn run() -> Result<(), Error> {
-    let credentials = try!(credentials::from_env());
-    let client = Client::new(credentials);
-
-    let response = try!(client.put("https://api.github.com/notifications", "{}"));
-    check(&response)
+fn credentials() -> Credentials {
+    match credentials::from_env() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("{}", e);
+            process::exit(1);
+        }
+    }
 }
 
-fn check(response: &Response) -> Result<(), Error> {
+fn run(client: &Client) -> Result<(), Error> {
+    let response = try!(client.put("https://api.github.com/notifications", "{}"));
+
     if response.status.is_success() {
         Ok(())
     } else {
